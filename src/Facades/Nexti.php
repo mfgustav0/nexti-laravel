470d7fd6de3533a90ef4bc8da51e8eafb1ea585a
Gustavo Monteiro Favaro <gustavomonteirofavaro@gmail.com>
2024-10-03 02:58:42 -0300

    Update Facade Class

--- src/Facades/Nexti.php
+++ src/Facades/Nexti.php
@@ -5,12 +5,18 @@ namespace Mfgustav0\Nexti\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Mfgustav0\Nexti\Nexti
+ * @method static \Mfgustav0\Nexti\Client withClientId(string|null $clientId)
+ * @method static \Mfgustav0\Nexti\Client withClientSecret(string|null $clientSecret)
+ * @method static \Mfgustav0\Nexti\Client withDebug()
+ * @method static \Mfgustav0\Nexti\Client withoutDebug()
+ * @method static \Mfgustav0\Nexti\Services\City city()
+ *
+ * @see \Mfgustav0\Nexti\Client
  */
 class Nexti extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return \Mfgustav0\Nexti\Nexti::class;
+        return \Mfgustav0\Nexti\Client::class;
     }
 }
